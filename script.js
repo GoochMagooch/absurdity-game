fd940f04533f6954a8fa432a6b446b144d0f5fa0
Magooch <crvallecito@gmail.com>
2024-06-22 20:42:23 -0600

    Trying to get program to do what I want

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 let randomNum = Math.random() * 5 + 1;
-const absurd = document.querySelector(".absurd");
-const lowOrHi = document.querySelector(".lowOrHi");
-const btnValue = document.querySelector(".btn-value");
-const btn = document.querySelector(".btn");
+let absurd = document.querySelector(".absurd");
+let lower = document.querySelector(".lower");
+let higher = document.querySelector(".higher");
+let btnValue = document.querySelector(".btn-value");
+let btn = document.querySelector(".btn");
 
-function tryYourLuck() {
-
-    btnValue.textContent = `Your number is: ${randomNum}`;
-    
-    if (randomNum === 3.141592653589793) {
-        absurd.textContent = "Congratulations you landed on PI, how absurd!"
-        setKillButton();
-    } else if (randomNum < 3.141592653589793) {
-        lowOrHi.textContent = "This number is less than PI, try again!";
-        resetTime();
-    } else {
-        lowOrHi.textContent = "This number is higher than PI, try again!"
-        resetTime();
-    }
-
-}
-
-btn.addEventListener("click", tryYourLuck);
-
+/*
 function setKillButton() {
     btn.disabled = true;
+}
+
+function resetButton() {
+    let resetParas = document.querySelectorAll(".resultParas");
+    for (let reset of resetParas) {
+        reset.textContent = "";
+    }
 }
 
 function resetTime() {
     document.body.append(resetButton)
     btn.addEventListener("click", resetButton)
 }
+*/
 
-function resetButton() {
-    const resetParas = document.querySelectorAll(".resultParas");
-    for (const reset of resetParas) {
-        reset.textContent = "";
+function tryYourLuck() {
+    
+    if (randomNum === 3.141592653589793) {
+        absurd.textContent = "Congratulations you landed on PI, how absurd!"
+        setKillButton();
+    } else if (randomNum < 3.141592653589793) {
+        lower.textContent = "This number is less than PI, try again!";
+        resetTime();
+    } else {
+        higher.textContent = "This number is higher than PI, try again!"
+        btnValue.textContent = `Your number is: ${randomNum}`;
+        resetTime();
     }
 }
+
+btn.addEventListener("click", tryYourLuck);
+btn.addEventListener('click', function handleClick() {
+    tryYourLuck();
+  });
 
 /*function tryYourLuckTwo() {
 
