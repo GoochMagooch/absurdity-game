8d86e72bf73cae3a18daa59df052e2448287e835
Magooch <crvallecito@gmail.com>
2025-02-26 17:22:53 -0700

    Set pi to 3.14

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let absurd = document.getElementById("absurd");
 let output = document.getElementById("output");
 let btnValue = document.getElementById("btn-value");
 let btn = document.getElementById("btn");
+let pi = 3.14
 let game = true
 
 function setKillButton() {
@@ -10,13 +11,13 @@ function setKillButton() {
 
 function tryYourLuck() {
     if (game === true) {
-        let randomNum = Math.random() * 4 + 1;
-        if (randomNum === 3.141592653589793) {
+        let randomNum = parseFloat(Math.random() * 3 + 1).toFixed(2)
+        if (randomNum === pi) {
             absurd.textContent = "Congratulations you landed on PI, how absurd!"
             btnValue.textContent = randomNum
             setKillButton();
-        } else if (randomNum < 3.141592653589793) {
-            output.textContent = "This number is less than PI, try again!";
+        } else if (randomNum < pi) {
+            output.textContent = "This number is less than PI, try again!"
             btnValue.textContent = `Your number is: ${randomNum}`
         } else {
             output.textContent = "This number is higher than PI, try again!"
